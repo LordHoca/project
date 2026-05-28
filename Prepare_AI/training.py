f9364e2414980b01efc3814d1b91084e6dd9a789
LordHoca <lordhoca1@gmail.com>
2026-05-28 12:42:02 +0700

    Update training parameters for YOLO model

--- Prepare_AI/training.py
+++ Prepare_AI/training.py
@@ -6,8 +6,8 @@ model = YOLO('yolo11n.pt')
 # 2. Mulai proses belajar
 model.train(
     data='bumbu_masak.yaml',    # File yaml yang kita buat tadi
-    epochs=25,            # Berapa kali model belajar ulang (bisa ditambah ke 100)
-    imgsz=512,            # Ukuran standar gambar YOLO
+    epochs=100,            # Berapa kali model belajar ulang (bisa ditambah ke 100)
+    imgsz=640,            # Ukuran standar gambar YOLO
     batch=4,             # Jumlah gambar per proses (sesuaikan dengan RAM/VRAM)
     patience=10,         # Berapa lama model menunggu sebelum berhenti jika tidak ada peningkatan
     name='model_bumbu_masak'    # Nama folder hasil training
